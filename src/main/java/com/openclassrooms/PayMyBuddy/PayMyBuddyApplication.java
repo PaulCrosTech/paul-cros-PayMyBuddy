c46aebd3f5a0341e3ccdde8084271718d80867a2
Paul CROS <paul.cros.tech@gmail.com>
2025-02-16 10:01:48 +0100

    Ajout slf4j & logback pour la gestion des logs

--- src/main/java/com/openclassrooms/PayMyBuddy/PayMyBuddyApplication.java
+++ src/main/java/com/openclassrooms/PayMyBuddy/PayMyBuddyApplication.java
@@ -1,13 +1,18 @@
 package com.openclassrooms.PayMyBuddy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayMyBuddyApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(PayMyBuddyApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
+		log.info("==> Application PayMyBuddy started <==");
 	}
 
 }
